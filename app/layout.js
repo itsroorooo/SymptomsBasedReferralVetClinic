27b2646f6b4e3e477f8372809dc4daae29da6ed9
Joy01387 <baqueranrose@gmail.com>
2025-03-22 21:01:22 +0800

    first commit

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "SymptoVet",
+  description: "Symptom-Based Refferal System for Domestic Pets",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
